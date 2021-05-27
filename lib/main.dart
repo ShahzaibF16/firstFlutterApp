1ff4e61daca16fba56e2da0d8856dc0cd7697f47
ShahzaibF16 <64368602+ShahzaibF16@users.noreply.github.com>
2021-05-27 16:13:00 +0500

    Flutter Assignment
    2nd assignment of flutter app development

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,83 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: (
-      Scaffold(
-        body: Text("Shahzaib Khalid"),
-      )
-    ),
-    
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: Colors.indigo[100],
+            appBar: AppBar(
+              elevation: 25,
+              shadowColor: Color(0xff374ABE),
+              backgroundColor: Color(0xff374ABE),
+              leading: Icon(Icons.account_circle_rounded),
+              title: Text(
+                'Login Page',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            body: SingleChildScrollView(
+              child: Center(
+                child: Column(children: [
+                  SizedBox(
+                    height: 220,
+                  ),
+                  Container(
+                    width: 320,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        suffixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(60.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    width: 320,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        suffixIcon: Icon(Icons.visibility_off),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(60.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  Container(
+                    height: 50.0,
+                    margin: EdgeInsets.all(10),
+                    child: RaisedButton(
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(80.0)),
+                      padding: EdgeInsets.all(0.0),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Color(0xff374ABE), Color(0xff64B6FF)],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                            borderRadius: BorderRadius.circular(30.0)),
+                        child: Container(
+                          constraints:
+                              BoxConstraints(maxWidth: 250.0, minHeight: 50.0),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Login",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
+              ),
+            )));
   }
 }
